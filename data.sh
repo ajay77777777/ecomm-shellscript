a98647892088f87468e1f68fcea68c7b7d867f12
ajay77777777 <141303594+ajay77777777@users.noreply.github.com>
2023-10-23 12:06:56 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,5 +1,7 @@
-git clone https://github.com/GOUSERABBANI44/flask-library-app.git
+git clone https://github.com/ajay77777777/ecomm.git
 cd flask-library-app
-sudo apt install -y python3-pip
-pip install -r requirements.txt
-python3 app.py
+sudo apt install -y apache2
+sudo systemctl start apache2
+sudo systemctl enable apache2
+sudo cp -r * /var/www/html
+sudo systemctl restart apache2
